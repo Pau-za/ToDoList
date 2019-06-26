97cf5011523b88345f180e09bc7e312526028e7c
Pau-za <molo_pau@hotmail.com>
2019-06-26 18:58:16 -0500

    first commit with vue and firebase config

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD6C1E8MyHPW1pdL0-872UmMulGW3xMXlA",
+  authDomain: "todo-list-vue-c19.firebaseapp.com",
+  databaseURL: "https://todo-list-vue-c19.firebaseio.com",
+  projectId: "todo-list-vue-c19",
+  storageBucket: "",
+  messagingSenderId: "1004892161319",
+  appId: "1:1004892161319:web:12d1026b9a3e6229"
+};
