29ce259a48aadad5d39381f12bffe4038520c790
Pau-za <molo_pau@hotmail.com>
2019-06-26 19:35:03 -0500

    completes config with vuex, store.js file, and views carpet

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,7 @@
+import firebase from 'firebase';
+import firestore from 'firebase/firestore';
+
+
 const firebaseConfig = {
   apiKey: "AIzaSyD6C1E8MyHPW1pdL0-872UmMulGW3xMXlA",
   authDomain: "todo-list-vue-c19.firebaseapp.com",
@@ -7,3 +11,8 @@ const firebaseConfig = {
   messagingSenderId: "1004892161319",
   appId: "1:1004892161319:web:12d1026b9a3e6229"
 };
+
+const fb = firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(fb);
+
+export default db;
---
+++ src/views/Start.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>This is my tasks list</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Start"
+};
+</script>
+
